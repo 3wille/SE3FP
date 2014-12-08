2a50c538d438428eb1862cb12aa0c7397f8b00e7
stryder12 <agbonghae@aol.com>
2014-12-08 11:14:16 +0100

    done Blatt 6

--- SE3FP_Blatt06_Smith_Heinrich_Wille.rkt
+++ SE3FP_Blatt06_Smith_Heinrich_Wille.rkt
@@ -28,4 +28,94 @@
  Es ist auch keine geschachtelte Rekursion, da die Funktion selbst nicht als Argument bei der rekursiven Verwendung benutzt wird.
  Die Funktion wird lediglich merge als Argument übergeben. Die Funktion ist nicht endrekursiv, weil das Ergebnis durch den Aufruf
  von merge noch mit einem anderen Term verknüpft werden muss.
-|#
+|# 
+
+(require 2htdp/image)
+;lang/pos nötig für die Positionen der einzelnen Teile des Bildes. 
+(require lang/posn)
+
+;ZEichnet einen Baum mit einem Stern auf der Spitze sowie mehrere Koch-Schneeflocken. Baum besteht aus Dreiecken und einem Viereck, Stern aus einem star-polygon. Da die Farbe der Flocken nicht intensiv genug war, 
+;haben wir mehrere Flocken übereinander gemalt.        
+(define (Bild) 
+  (place-images/align
+   (list (star-polygon 40 5 2 "solid" "gold")
+         (triangle 50 "solid" "darkgreen")
+         (triangle 70 "solid" "darkgreen")
+         (triangle 90 "solid" "darkgreen")
+         (triangle 100 "solid" "darkgreen")
+         (rectangle 20 60 "solid" "brown")
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+         flocke
+          flocke
+         flocke)
+   (list (make-posn 100 40)
+         (make-posn 100 78)
+         (make-posn 100 100)
+         (make-posn 100 130)
+         (make-posn 100 160)
+         (make-posn 100 200)
+         (make-posn 50 100)
+         (make-posn 50 80)
+         (make-posn 150 50)
+         (make-posn 150 100)
+         (make-posn 150 100)
+         (make-posn 150 100)
+         (make-posn 150 100)
+         (make-posn 150 100)
+         (make-posn 50 100)
+         (make-posn 50 80)
+         (make-posn 150 50)
+         (make-posn 150 100)
+         (make-posn 150 100)
+         (make-posn 150 100)
+         (make-posn 150 100)
+         (make-posn 150 100)
+         (make-posn 50 100)
+         (make-posn 50 80)
+         (make-posn 50 100)
+         (make-posn 50 80)
+         )
+   "center" "top"
+   (rectangle 300 300 "solid" "white"))
+  )
+
+
+
+;Hilfsfunktion, um die Flocken zu malen
+(define (koch-kurve n)
+    (cond
+      [(zero? n) (square 0.04 "solid" "black")]
+      [else
+       (local [(define smaller (koch-kurve (- n 1)))]
+         (beside/align "bottom"
+                       smaller
+                       (rotate 60 smaller)
+                       (rotate -60 smaller)
+                       smaller))]))
+;Funktion, um mit der Hilfsfunktion die Flocken zusammenzusetzen
+(define flocke
+  (above
+   (beside
+    (rotate 60 (koch-kurve 5))
+    (rotate -60 (koch-kurve 5)))
+   (flip-vertical (koch-kurve 5))))
+
+;Aufruf der Funktion Bild, die das Bild malt
+(Bild)
+
